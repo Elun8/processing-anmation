8ac523f3ee5b7f3decd36aa454c7116c30d803e2
Elun8 <36413807+Elun8@users.noreply.github.com>
2018-04-25 10:45:38 -0400

    Update landscape.java

--- landscape.java
+++ landscape.java
@@ -1,1 +1,28 @@
+int x;
 
+void setup() {
+  size(600, 600);
+}
+
+void draw() {
+  background(#87CEEB);
+  
+  
+  noStroke();
+  fill(#8B4513);
+  rect(0, height-400, width, 400);
+  
+    noStroke();
+    fill(#FDFD13);
+    ellipse(width-100, 100, 75, 75);
+  
+    fill(255, 0, 115);
+    rect(x, 200, 100, 20);
+    rect(x + 15, 178, 70, 40);
+    
+    fill(77, 66, 66);
+    ellipse(x + 25, 221, 24, 24);
+    ellipse(x + 75, 221, 24, 24);
+    
+    x = x + 2;
+}
