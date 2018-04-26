9139da56b42d667ed281b7afb05c3d3e12dd2aa9
Elun8 <36413807+Elun8@users.noreply.github.com>
2018-04-26 10:46:28 -0400

    Update landscape.java

--- landscape.java
+++ landscape.java
@@ -4,22 +4,26 @@ void setup() {
   size(600, 600);
 }
 
+//background
 void draw() {
   background(#87CEFA);
   
-  
+  //ground
   noStroke();
   fill(#8B4513);
   rect(0, height-385, width, 385);
   
+    //sun
     noStroke();
     fill(#FDFD13);
     ellipse(width-100, 100, 75, 75);
-  
+    
+    //car  
     fill(255, 0, 115);
     rect(x, 200, 100, 20);
     rect(x + 15, 178, 70, 40);
     
+    //wheels
     fill(77, 66, 66);
     ellipse(x + 25, 221, 24, 24);
     ellipse(x + 75, 221, 24, 24);
